a066c9819178194e1994a78c5b3126a31948c3db
Anton <pozernishku@gmail.com>
2019-09-16 02:29:26 +0300

    simple http test

--- srv.py
+++ srv.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 
 # BaseHTTPRequestHandler
-from http.server import HTTPServer, BaseHTTPRequestHandler
+# from http.server import HTTPServer, BaseHTTPRequestHandler
 
-def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+# def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+#     server_address = ('', 8000)
+#     httpd = server_class(server_address, handler_class)
+#     httpd.serve_forever()
 
 # 85.159.4.138
 
 
 # SimpleHTTPRequestHandler
-# import http.server
-# import socketserver
+import http.server
+import socketserver
 
-# PORT = 8000
+PORT = 8000
 
-# Handler = http.server.SimpleHTTPRequestHandler
+Handler = http.server.SimpleHTTPRequestHandler
 
-# with socketserver.TCPServer(("", PORT), Handler) as httpd:
-#     print("serving at port", PORT)
-#     httpd.serve_forever()
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
