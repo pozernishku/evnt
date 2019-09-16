3fa62cdf65bb27b3f438a0e186aa601f5b43a714
Anton <pozernishku@gmail.com>
2019-09-16 16:15:28 +0300

    2 - 3 points

--- srv.py
+++ srv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Digital Ocean IP 85.159.4.138
+# Digital Ocean IP 85.159.4.138:8000
 
 # BaseHTTPRequestHandler
 from http.server import HTTPServer, BaseHTTPRequestHandler
@@ -8,11 +8,10 @@ from io import BytesIO
 import json
 
 class HTTPReqHandler(BaseHTTPRequestHandler):
-
-    # def do_GET(self):
-    #     self.send_response(200)
-    #     self.end_headers()
-    #     self.wfile.write(b'Hello, world!')
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'TODO GET response!')
 
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
@@ -22,8 +21,24 @@ class HTTPReqHandler(BaseHTTPRequestHandler):
         response = BytesIO()
         response.write(body)
 
-        # TODO
+        # Task
         data = json.loads(response.getvalue())
+
+        # 1. Prefixes sg_ removing
+        for dic in data:
+            for d in dic:
+                if d.startswith('sg_'):
+                    dic[d[3:]] = dic.pop(d)
+
+        # 2. Predefined fields removing
+        for dic in data:
+            for k in ['useragent', 'tls', 'smtp-id', 'url_offset', 'cert_err']:
+                dic.pop(k, None)
+
+        # 3. MongoDB saving TODO...
+
+        # 4. 
+
         print(data)
 
         self.wfile.write(response.getvalue())
