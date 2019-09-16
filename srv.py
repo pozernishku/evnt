a897313bd1902b5733e793cc6d4c3bdd08ec0fdd
Anton <pozernishku@gmail.com>
2019-09-16 03:10:04 +0300

    do_POST

--- srv.py
+++ srv.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python3
 
+# Digital Ocean IP 85.159.4.138
+
 # BaseHTTPRequestHandler
-# from http.server import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
 
-# def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
-#     server_address = ('', 8000)
-#     httpd = server_class(server_address, handler_class)
-#     httpd.serve_forever()
+class HTTPReqHandler(BaseHTTPRequestHandler):
 
-# 85.159.4.138
+    # def do_GET(self):
+    #     self.send_response(200)
+    #     self.end_headers()
+    #     self.wfile.write(b'Hello, world!')
 
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        self.send_response(200)
+        self.end_headers()
+        response = BytesIO()
+        response.write(b'This is POST request. ')
+        response.write(b'Received: ')
+        response.write(body)
+        self.wfile.write(response.getvalue())
+
+def run(server_class=HTTPServer, handler_class=HTTPReqHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    print("serving at port 8000")
+    run()
 
 # SimpleHTTPRequestHandler
-import http.server
-import socketserver
+# import http.server
+# import socketserver
 
-PORT = 8000
+# PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
+# Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+# with socketserver.TCPServer(("", PORT), Handler) as httpd:
+#     print("serving at port", PORT)
+#     httpd.serve_forever()
